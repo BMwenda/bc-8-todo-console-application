a8ef6dc38cacd42ed4392d1d2bca86efaebb7310
David Njakai <dvdseroney@gmail.com>
2016-06-24 01:43:58 +0300

    change to second db connection and cursor for ToDoItem objects

--- todo_item.py
+++ todo_item.py
@@ -1,4 +1,4 @@
-from database import conn, c
+from database import conn2, c2
 
 class ToDoItem(object):
 	"""docstring for ToDoItem"""
@@ -8,17 +8,17 @@ class ToDoItem(object):
 
 	def save_item(self, listname):
 		SQL = "INSERT INTO todoitem (CONTENT, COMPLETE, LISTNAME) VALUES ('{0}', '{1}', '{2}')"
-		c.execute(SQL.format(self.content, self.complete, listname))
-		conn.commit()
+		c2.execute(SQL.format(self.content, self.complete, listname))
+		conn2.commit()
 
 	def delete_item(self, listname):
 		SQL = "DELETE FROM todoitem WHERE CONTENT = '{0}' AND LISTNAME = '{1}'".format(self.content, listname)
-		c.execute(SQL)
-		conn.commit()
+		c2.execute(SQL)
+		conn2.commit()
 
 	def get_items(self, listname):
-		SQL = "SELECT CONTENT, COMPLETE FROM todoitem WHERE LISTNAME = '{0}'".format(self.listname)
-		return c.execute(SQL)
+		SQL = "SELECT CONTENT, COMPLETE FROM todoitem WHERE LISTNAME = '{0}'".format(listname)
+		return c2.execute(SQL)
 
 	def edit_item(self, content):
 		self.content = content
