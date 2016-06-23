0487ca7bdaa1851ae73bc1be5e83f9b6641c1048
David Njakai <dvdseroney@gmail.com>
2016-06-23 23:15:05 +0300

    add database.py, remove in-class data type testing and separate modules
    testing will be handled by myapp.py. Separation of database module will
    enhance readability when using class methods to interact with database

--- todo_item.py
+++ todo_item.py
@@ -1,8 +1,8 @@
-import todo_item
-import sqlite3
+from database import conn, c
+
 class ToDoItem(object):
 	"""docstring for ToDoItem"""
-	def __init__(self, content = '1st item', complete = 0, *args):
+	def __init__(self, content = '1st item', complete = 0):
 		if type(content) != type(''):
 			self.content = 'Invalid content'
 		else:
@@ -15,19 +15,18 @@ class ToDoItem(object):
 		self.args = args
 
 	def save_item(self, listname):
-		conn = sqlite3.connect('crollodb.db')
-		c = conn.cursor()
 		SQL = "INSERT INTO todoitem (CONTENT, COMPLETE, LISTNAME) VALUES ('{0}', '{1}', '{2}')"
 		c.execute(SQL.format(self.content, self.complete, listname))
 		conn.commit()
-		conn.close()
 
 	def delete_item(self, listname):
-		conn = sqlite3.connect('crollodb.db')
-		c = conn.cursor()
 		SQL = "DELETE FROM todoitem WHERE CONTENT = '{0}' AND LISTNAME = '{1}'".format(self.content, listname)
 		c.execute(SQL)
 		conn.commit()
-		conn.close()
 
+	def update_item(self, content):
+		pass
+
+	def finish_item(self):
+		self.complete = 1
 		
--- todo_list.py
+++ todo_list.py
@@ -1,38 +1,15 @@
 import todo_item
-import sqlite3
-
+from database import conn, c
 
 class ToDoList(object):
 	def __init__(self, name, description = 'A generic to do list', todo_items = [todo_item.ToDoItem()]):
-		if type(name) != type(''):
-			self.name = 'Enter valid name'
-		else:
-			self.name = name
-		if type(description) != type(''):
-			self.description = 'Enter valid description'
-		else:
-			self.description = description
+		self.name = name
+		self.description = description
 		self.todo_items = todo_items
 
-	def add_todo(self, content, complete = 0, *args):
-		if type(complete) != type(1):
-			self.complete = 0
-			return
-		if type(content) != type(''):
-			return 'Enter valid content'
-		item = todo_item.ToDoItem(content, complete, *args)
+	def add_todo(self, content, complete = 0):
+		item = todo_item.ToDoItem(content, complete)
 		self.todo_items.append(item)
-
-	def finish_item(self, index):
-		if index >= len(self.todo_items) or index < 0:
-			return 'That to do item does not exist'
-		self.todo_items[index].complete = 1
-		conn = sqlite3.connect('crollodb.db')
-		c = conn.cursor()
-		SQL = "UPDATE todoitem SET COMPLETE = 1 WHERE LISTNAME = '{0}' AND CONTENT = '{1}'"
-		c.execute(SQL.format(self.name, self.todo_items[index].content))
-		conn.commit()
-		conn.close()
 
 	def edit_item(self, index, content):
 		self.todo_items[index] = content
@@ -49,22 +26,16 @@ class ToDoList(object):
 		return percentage
 
 	def save_list(self):
-		conn = sqlite3.connect('crollodb.db')
-		c = conn.cursor()
 		for item in self.todo_items:
 			item.save_item(self.name)
 		SQL = "INSERT INTO todolist (NAME, DESCRIPTION) VALUES ('{0}', '{1}')"
 		c.execute(SQL.format(self.name, self.description))
 		conn.commit()
-		conn.close()
 
 	def delete_list(self):
-		conn = sqlite3.connect('crollodb.db')
-		c = conn.cursor()
 		if len(self.todo_items) > 0:
 			for item in self.todo_items:
 				item.delete_item(self.name)
 		SQL = "DELETE FROM todolist WHERE NAME = '{0}'".format(self.name)
 		c.execute(SQL)
 		conn.commit()
-		conn.close()
