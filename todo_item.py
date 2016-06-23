b7166a107fce673b07af6e196eb088e57de58e6f
David Njakai <dvdseroney@gmail.com>
2016-06-24 01:07:30 +0300

    get rid of in-class data type testing and add methods for getting items from database and editing object content

--- todo_item.py
+++ todo_item.py
@@ -3,16 +3,8 @@ from database import conn, c
 class ToDoItem(object):
 	"""docstring for ToDoItem"""
 	def __init__(self, content = '1st item', complete = 0):
-		if type(content) != type(''):
-			self.content = 'Invalid content'
-		else:
-			self.content = content
-			
-		if type(complete) != type(1):
-			self.complete = 0
-		else:
-			self.complete = complete
-		self.args = args
+		self.content = content
+		self.complete = complete
 
 	def save_item(self, listname):
 		SQL = "INSERT INTO todoitem (CONTENT, COMPLETE, LISTNAME) VALUES ('{0}', '{1}', '{2}')"
@@ -24,8 +16,12 @@ class ToDoItem(object):
 		c.execute(SQL)
 		conn.commit()
 
-	def update_item(self, content):
-		pass
+	def get_items(self, listname):
+		SQL = "SELECT CONTENT, COMPLETE FROM todoitem WHERE LISTNAME = '{0}'".format(self.listname)
+		return c.execute(SQL)
+
+	def edit_item(self, content):
+		self.content = content
 
 	def finish_item(self):
 		self.complete = 1
